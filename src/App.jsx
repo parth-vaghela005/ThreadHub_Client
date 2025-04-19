928996ce2aca9b6a457d7d63aaca25f5a7b6cc61
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-04-19 22:54:50 +0530

    Refactor App component structure and remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// ThreadHub
+
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import MainLayout from '@/layout/MainLayout'
+import Profile from '@/Pages/Profile'
+
+
 function App() {
-
-
   return (
-    <>
-    
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/:username/profile" element={<Profile />} />
+      </Route>
+    </Routes>
   )
 }
 
