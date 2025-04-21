adef4b5374db55ee5bebd9cbd5cb1c2c6ac693b3
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-04-21 22:55:56 +0530

    Add routing for CreatePostModal and ImageCaptionCard; integrate Toaster for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Routes, Route } from 'react-router-dom'
 import MainLayout from '@/layout/MainLayout'
 import Profile from '@/Pages/Profile'
 import Home from '@/Pages/Home'
+import ImageCaptionCard from '../src/Pages/ImageCaptionCard'
+import CreatePostModal from '.././src/Pages/CreatePostModal'
+// import {CreatePostModal} from '@Pages/CreatePostModal'
+// import ImageCaptionCard from '@Pages/ImageCaptionCard'
 // import Home from '@Pages/Home'
 // import Thread from '@/components/Thread'
 
@@ -11,8 +15,10 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
+        <Route path="home" element={<Home />} />    
+        <Route path="create-post" element={<CreatePostModal />} />
         <Route path="/:username/profile" element={<Profile />} />
+        <Route path="/generate" element={<ImageCaptionCard />} />
       </Route>
     </Routes>
   )
